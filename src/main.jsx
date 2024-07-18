5245cf7a5c8d65bad3d852565a6866a3286c2bad
hoangdinhnho23 <hellocunconhaha@gmail.com>
2024-07-18 20:38:09 +0700

    co len

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,20 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/movix/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           return id
+  //             .toString()
+  //             .split("node_modules/")[1]
+  //             .split("/")[0]
+  //             .toString();
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
 });
